18a4ab48aa8dabfefa9e06c31f30f615cb87cb15
Aaronhdez <aaronhdezalv@hotmail.com>
2022-03-16 12:05:41 +0000

    assets: Main menu added and linked to Pong view

---
+++ Scripts/SceneSelector.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour {
+    public void LoadPongScene() {
+        SceneManager.LoadScene("MainScenePong");
+    }
+
+    public void LoadArkanoidsScene() {
+
+    }
+
+    public void LoadBreakoutScene() {
+
+    }
+}
