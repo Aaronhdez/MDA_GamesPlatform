8be89aef83c752ff7b0ba0e80721cd8cb6372dca
Aaronhdez <aaronhdezalv@hotmail.com>
2022-03-18 10:23:22 +0000

    ballController added

--- Pong/Scripts/BallControllerPong.cs
+++ Pong/Scripts/BallControllerPong.cs
@@ -8,8 +8,6 @@ public class BallControllerPong : MonoBehaviour
     [SerializeField] private float initialVelocity = 4f;
     [SerializeField] private float multiplier = 1.1f;
     private Rigidbody2D ballRb;
-
-    // Start is called before the first frame update
     void Start()
     {
         ballRb = GetComponent<Rigidbody2D>();
