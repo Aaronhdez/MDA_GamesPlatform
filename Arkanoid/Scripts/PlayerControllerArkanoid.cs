06fc3b35572c70231137c77d2334d7eebca77413
GabrielHernandez99 <gabriel090199@gmail.com>
2022-03-21 20:16:50 +0000

    green: PlayerControllerArkanoid movement

--- Arkanoid/Scripts/PlayerControllerArkanoid.cs
+++ Arkanoid/Scripts/PlayerControllerArkanoid.cs
@@ -6,11 +6,18 @@ using UnityEngine;
 public class PlayerControllerArkanoid : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float upperLimit;
+    [SerializeField] private float lowerLimit;
+    [SerializeField] private float leftLimit;
+    [SerializeField] private float rightLimit;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        upperLimit = GameObject.Find("UpperBoundary").transform.position.y - 1;
+        lowerLimit = GameObject.Find("LowerBoundary").transform.position.y + 1;
+        leftLimit = GameObject.Find("LeftBoundary").transform.position.x + 1;
+        rightLimit = GameObject.Find("RightBoundary").transform.position.x - 1;
     }
 
     // Update is called once per frame
@@ -21,12 +28,13 @@ public class PlayerControllerArkanoid : MonoBehaviour
 
     private void MovePlayer()
     {
-        var input = Input.GetAxisRaw("Vertical");
-        var upperLimit = GameObject.Find("UpperBoundary").transform.position.y -1;
-        var lowerLimit = GameObject.Find("LowerBoundary").transform.position.y +1;
         Vector2 playerPosition = transform.position;
-        float movement = input * speed * Time.deltaTime;
-        playerPosition.y = Mathf.Clamp(playerPosition.y + movement, lowerLimit, upperLimit);
+        var inputY = Input.GetAxisRaw("Vertical");
+        var inputX = Input.GetAxisRaw("Horizontal");
+        float movementY = inputY * speed * Time.deltaTime;
+        float movementX = inputX * speed * Time.deltaTime;
+        playerPosition.y = Mathf.Clamp(playerPosition.y + movementY, lowerLimit, upperLimit);
+        playerPosition.x = Mathf.Clamp(playerPosition.x + movementX, leftLimit, rightLimit);
         transform.position = playerPosition;
     }
 }
