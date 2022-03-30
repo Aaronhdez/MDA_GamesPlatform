01ba1c100d9a4f557cdc117c9146060758e1d16b
Aaronhdez <aaronhdezalv@hotmail.com>
2022-03-30 10:49:55 +0100

    red: Score Controller added

---
+++ Pong/Scripts/ScoreControllerPong.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreControllerPong : MonoBehaviour
+{
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
+    public TextMeshProUGUI playerOneScoreText;
+    public TextMeshProUGUI playerTwoScoreText;
+    public Object sceneController;
+
+    void Start() {
+        playerOneScore = 0;
+        playerOneScoreText.SetText(playerOneScore.ToString());
+        playerTwoScore = 0;
+        playerTwoScoreText.SetText(playerTwoScore.ToString());
+    }
+}
