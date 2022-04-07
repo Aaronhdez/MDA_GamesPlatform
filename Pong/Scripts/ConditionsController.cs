b54bf42afe9327ac9d002db7fe10247ec97a012a
Aaronhdez <aaronhdezalv@hotmail.com>
2022-04-07 10:54:25 +0100

    green: SetVictoryConditions

--- Pong/Scripts/ConditionsController.cs
+++ Pong/Scripts/ConditionsController.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class ConditionsController : MonoBehaviour
 {
-    public Dropdown dropdown;
+    public TMP_Dropdown dropdown;
 
-    public void SetVictoryConditions() { 
-
+    public void SetVictoryConditions() {
+        int points = int.Parse(dropdown.captionText.text);
+        PlayerPrefs.SetInt("maxPoints", points);
+        Debug.Log(PlayerPrefs.GetInt("maxPoints"));
     }
 }
